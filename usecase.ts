84f369b078b675637ee8501fbc0e5bdffcf6475a
Bastian Paskal Situmorang <bastianpaskal@gmail.com>
2020-06-18 23:33:13 +0700

    feat: hash password

--- usecase.ts
+++ usecase.ts
@@ -1,3 +1,5 @@
+import { createHash } from "https://deno.land/std/hash/mod.ts";
+
 import { Account, ErrEmailDuplicate } from "./entity.ts"
 import { signUpRepo, signInRepo, existEmailRepo } from './repository/account.ts'
 
@@ -9,11 +11,19 @@ const signUp = async (account: Account) => {
         }
     }
 
+    const hash = createHash("md5");
+    hash.update(account.password);
+    account.password = hash.toString()
+
     const result = await signUpRepo(account)
     return result
 }
 
 const signIn = async (account: Account) => {
+    const hash = createHash("md5");
+    hash.update(account.password);
+    account.password = hash.toString()
+
     const result = await signInRepo(account)
     return result
 }
